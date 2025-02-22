0023b95d9ddbd3fa1b9149fee08facc6f70313d5
Kinglos01 <carlos.a.berio@hotmail.com>
2025-02-22 14:36:47 -0500

    Added a counter for the pages and a exit condition if the program goes over 100 pgs.  Also added the proper print conditions

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <random>
-
+#include <ctime>
 
 void userMemoryAllocation(double mem_arr[],double proc_arr[],double mem_pgs_arr[],double pgs_to_MB[]) {
 
@@ -9,6 +9,7 @@ void userMemoryAllocation(double mem_arr[],double proc_arr[],double mem_pgs_arr[
     int processID = 0;
     double ProcessAddress = 2000;
     double unusedSpace = 0;
+    double memPagesCounter = 0;
 
     for(int i = 0; i <= 99; i++) {
         randomNumber = rand() % 30 + 1;
@@ -19,12 +20,22 @@ void userMemoryAllocation(double mem_arr[],double proc_arr[],double mem_pgs_arr[
         pgs_to_MB[i] = (mem_pgs_arr[i]*160);
         ProcessAddress = ProcessAddress + pgs_to_MB[i];
         unusedSpace = pgs_to_MB[i] - proc_arr[i];
+         memPagesCounter =  memPagesCounter + mem_pgs_arr[i];
+    if (memPagesCounter >= 101) {break;}
 
+        std::cout << "-------------------------------------------------------------------------"  <<std::endl;
+        std::cout << "| Process ID " << "| Starting Memory Address " << "| Size of Process "<<"| Unused Space |" <<std::endl;
+        std::cout << "-------------------------------------------------------------------------"  <<std::endl;
+        std::cout <<  "|\t" << processID << "   |\t\t" << ProcessAddress << "   \t|\t" << proc_arr[i] << "\t|\t" << unusedSpace << "\t|" <<std::endl;
+        std::cout << "-------------------------------------------------------------------------"  <<std::endl;
+        std::cout << std::endl;
 
+        /*
         std::cout <<"Process ID is: "<< processID << "\nRandom array num is: "<< mem_arr[i]
         << "\nProcess size: "<< proc_arr[i] << " MB\nMemory Pages: "<< mem_pgs_arr[i]
         << "\nPages in MB: "<< pgs_to_MB[i]<<"\nProcess Address: "<< ProcessAddress<<"\nUnused Space: "
-        << unusedSpace <<"\n--------- "<<std::endl;
+        << unusedSpace <<"\nCount of pages: "<< memPagesCounter <<"\n--------- "<<std::endl;
+        */
     }
 
 }
