30d4bf4cf6aeb316f0166715427be6c2937e2f3c
Kinglos01 <carlos.a.berio@hotmail.com>
2025-02-22 14:38:52 -0500

    Added a counter for the pages and a exit condition if the program goes over 100 pgs.  Also added the proper print conditions

---
+++ cmake-build-debug/CMakeFiles/OS_Program_asignment_1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OS_Program_asignment_1.dir/main.cpp.obj"
+  "CMakeFiles/OS_Program_asignment_1.dir/main.cpp.obj.d"
+  "OS_Program_asignment_1.exe"
+  "OS_Program_asignment_1.exe.manifest"
+  "OS_Program_asignment_1.pdb"
+  "libOS_Program_asignment_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OS_Program_asignment_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
